b49895e98d38bf0ab736a764e629a6d71cb34f9d
Caiosouzadevv <caioss20.cs@gmail.com>
2024-06-21 06:22:08 -0300

    [FIX]leaving dynamic components

--- src/app/components/card-item/card-item.component.ts
+++ src/app/components/card-item/card-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-item',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './card-item.component.scss'
 })
 export class CardItemComponent {
+@Input() text: string = '';
+@Input() value: string = '';
 
 }
